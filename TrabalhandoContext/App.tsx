fafdd3e3f0b451183c260d3069507016ed67c404
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-04 09:20:33 -0300

    Instalando dependências

--- TrabalhandoContext/App.tsx
+++ TrabalhandoContext/App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './src/screens/HomeScreen';
+import UserScreen from './src/screens/UserScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
