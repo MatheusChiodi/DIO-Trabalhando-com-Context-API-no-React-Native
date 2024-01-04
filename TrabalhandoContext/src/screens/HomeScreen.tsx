97108bf11359ea0e23fd0721ad2fdf2d09cbff4a
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-04 09:37:11 -0300

    Criando o HomeScreen

--- TrabalhandoContext/src/screens/HomeScreen.tsx
+++ TrabalhandoContext/src/screens/HomeScreen.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function HomeScreen() {
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../App';
+
+type HomeScreenProps = {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+};
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
+  const [name, setName] = useState('');
+
+  const navigateToUserScreen = () => {
+    navigation.navigate('User', { userName: name });
+  };
+
   return (
     <View>
-      <Text>Home</Text>
+      <TextInput
+        placeholder="Digite seu nome"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+
+      <Button title="Ir para tela de usuário" onPress={navigateToUserScreen} />
     </View>
   );
 }
